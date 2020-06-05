257e3e703c031362be134d97c5d6cab9630df73c
Tiago Cordeiro <tiago@mulhergorila.com>
2020-06-05 19:02:11 -0300

    Bump version

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="dj3-cloudinary-storage",
-    version="0.0.2",
+    version="0.0.3",
     author="Tiago Cordeiro",
     author_email="tiago@mulhergorila.com",
     description=description,
